2cbdd55e41d62b11fe1882ea0f59ab5644a0ea29
Yasir Tahir <yasirtahir1999@gmail.com>
2020-09-05 08:43:46 -0500

    First Version of Hello

---
+++ Hello.java
@@ -0,0 +1,18 @@
+// Yasir Tahir 20200904
+/*
+This program simply just says Hello.
+It is our first Java program of the semester.
+*/
+/** 
+This is a javadoc comment.
+We'll talk about these in the future.
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+
+	}
+}
+
